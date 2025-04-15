7a56aee8b2d134dbf81716afbcd84b2025f05b55
George Hyde <georgewhyde3@gmail.com>
2025-04-14 20:12:36 -0500

    initial tests

--- MealPlannerProject/MealPlan.Tests/PantryClassTests.cs
+++ MealPlannerProject/MealPlan.Tests/PantryClassTests.cs
@@ -1,1 +1,69 @@
-namespace MealPlan.Tests;
+namespace MealPlan.Tests
+{
+    using Xunit;
+    using MealPlan; // Assuming Pantry and Ingredient classes are in this namespace
+    using System.Collections.Generic;
+    using System.Linq; // Used for Assert.Empty, Assert.Single, Assert.Contains
+
+    public class PantryTests
+    {
+        // Helper method to create sample ingredients easily for tests
+        private Ingredient CreateIngredient(string name, int amount = 1, string measure = "unit", string category = "TestCategory")
+        {
+            return new Ingredient
+            {
+                name = name,
+                amount = amount,
+                measurementType = measure,
+                foodCategory = category
+            };
+        }
+
+        [Fact]
+        public void AddIngredientsToPantryFromShoppingList_NullList_ReturnsZero()
+        {
+            var pantry = new Pantry();
+            List<Ingredient> listToAdd = null;
+
+            int countAdded = pantry.AddIngredientsToPantryFromShoppingList(listToAdd);
+
+            Assert.Equal(0, countAdded); 
+            Assert.Empty(pantry.Ingredients);
+        }
+
+        [Fact]
+        public void AddIngredientsToPantryFromShoppingList_SingleItem_AddsItemAndReturnsOne()
+        {
+            var pantry = new Pantry();
+            var ingredient = CreateIngredient("Milk", 1, "gallon", "Dairy");
+            var listToAdd = new List<Ingredient> { ingredient };
+
+            int countAdded = pantry.AddIngredientsToPantryFromShoppingList(listToAdd);
+
+            Assert.Equal(1, countAdded);
+            Assert.Single(pantry.Ingredients); 
+            Assert.Contains(ingredient, pantry.Ingredients); 
+        }
+
+        [Fact]
+        public void AddIngredientsToPantryFromShoppingList_ToExistingPantry_AppendsItems()
+        {
+            var pantry = new Pantry();
+            var initialIngredient = CreateIngredient("Butter", 1, "stick", "Dairy");
+            pantry.Ingredients.Add(initialIngredient); 
+
+            var ingredientToAdd1 = CreateIngredient("Flour", 5, "lbs", "Baking");
+            var ingredientToAdd2 = CreateIngredient("Sugar", 2, "lbs", "Baking");
+            var listToAdd = new List<Ingredient> { ingredientToAdd1, ingredientToAdd2 };
+
+            int countAdded = pantry.AddIngredientsToPantryFromShoppingList(listToAdd);
+
+            Assert.Equal(2, countAdded); 
+            Assert.Equal(3, pantry.Ingredients.Count); 
+            Assert.Contains(initialIngredient, pantry.Ingredients); 
+            Assert.Contains(ingredientToAdd1, pantry.Ingredients); 
+            Assert.Contains(ingredientToAdd2, pantry.Ingredients); 
+        }
+
+    }
+}
--- MealPlannerProject/MealPlan.Tests/RecipeListClassTests.cs
+++ MealPlannerProject/MealPlan.Tests/RecipeListClassTests.cs
@@ -1,1 +1,57 @@
-namespace MealPlan.Tests;
+namespace MealPlan.Tests
+{
+    using Xunit;
+    using MealPlan; 
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RecipeListTests
+    {
+        private Recipe CreateRecipe(string name)
+        {
+            return new Recipe
+            {
+                Name = name,
+            };
+        }
+
+        [Fact]
+        public void RecipeList_Constructor_ShouldInitializeEmptyList()
+        {
+            var recipeList = new RecipeList();
+
+            Assert.NotNull(recipeList.Recipes); 
+            Assert.Empty(recipeList.Recipes);   
+        }
+
+        [Fact]
+        public void AddRecipeToRecipeList_AddsRecipeToEmptyList()
+        {
+            var recipeList = new RecipeList();
+            var recipe = CreateRecipe("Pasta Bake");
+
+            recipeList.AddRecipeToRecipeList(recipe);
+
+            Assert.Single(recipeList.Recipes); 
+            Assert.Contains(recipe, recipeList.Recipes); 
+            Assert.Same(recipe, recipeList.Recipes[0]); 
+        }
+
+        [Fact]
+        public void AddRecipeToRecipeList_AddsRecipeToExistingList()
+        {
+            var recipeList = new RecipeList();
+            var initialRecipe = CreateRecipe("Omelette");
+            recipeList.Recipes.Add(initialRecipe); 
+
+            var recipeToAdd = CreateRecipe("Salad");
+
+            recipeList.AddRecipeToRecipeList(recipeToAdd);
+
+            Assert.Equal(2, recipeList.Recipes.Count); 
+            Assert.Contains(initialRecipe, recipeList.Recipes);
+            Assert.Contains(recipeToAdd, recipeList.Recipes);
+            Assert.Same(recipeToAdd, recipeList.Recipes[1]); 
+        }
+    }
+}
